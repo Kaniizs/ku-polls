e93b4a69dc1d2d68b6cd90c4345369a172dfff54
Mojizsz <isaraa.p@ku.th>
2022-09-10 20:09:17 +0700

    Adding an optional Django part 7 and adding a Qustion method.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date', 'end_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date',
+                    'was_published_recently', 'can_vote', 'is_published')
+    list_filter = ['pub_date', 'end_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,13 +8,31 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date ended', null=True)
+    end_date = models.DateTimeField('date ended', null=True, default=None)
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        """Return a boolean whenever the question was published recently"""
+        return timezone.localtime() - datetime.timedelta(days=1) <= self.pub_date <= timezone.localtime()
+
+    def is_published(self):
+        """Return a boolean whenever the question was published"""
+        return self.pub_date <= timezone.localtime()
+
+    def can_vote(self):
+        """Return a boolean whenever the question is allowed to vote"""
+        if self.end_date is None:
+            return timezone.localtime() >= self.pub_date
+        return self.end_date >= timezone.localtime() >= self.pub_date
 
     def __str__(self):
+        """Return a Question text"""
         return self.question_text
-    
-    def was_published_recently(self):
-        return timezone.localtime() - datetime.timedelta(days=1) <= self.pub_date <= timezone.localtime()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,4 +40,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """Return a choice text"""
         return self.choice_text
